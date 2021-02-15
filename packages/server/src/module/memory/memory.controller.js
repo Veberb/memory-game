5239e63fc5e39deaf14683fbc3799ef93374cad0
Lucas Veber <lucasveberdebrida@gmail.com>
2021-02-15 02:05:22 -0200

    remove console.log

--- packages/server/src/module/memory/memory.controller.js
+++ packages/server/src/module/memory/memory.controller.js
@@ -7,8 +7,6 @@ module.exports = app => {
 }
 
 router.get('/random/:length', async (req, res, next) => {
-  console.log('oi')
-  console.log(req.params)
   const random = await memoryManager.getRandomNumbersList({ ...req.params })
   return res.json(random)
 })
